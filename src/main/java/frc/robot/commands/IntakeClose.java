9433ea0c4bdf091c32a8e028be25b50942333ed1
madpikachu97 <johnadare3@gmail.com>
2024-01-20 12:00:40 -0600

    completed intakeClose.java

--- src/main/java/frc/robot/commands/IntakeClose.java
+++ src/main/java/frc/robot/commands/IntakeClose.java
@@ -5,16 +5,19 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystem.Pneumatics;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class IntakeClose extends InstantCommand {
-  public IntakeClose() {
+  public IntakeClose(Pneumatics pneumatics) {
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+pneumatics.Close();
+  }
 }
