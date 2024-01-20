83a18a6c5255c56f90fae087e2b9d6f00b92aaca
Kylejackson27 <kyle.jackson480@gmail.com>
2024-01-20 10:37:10 -0600

    changed constants and made shooterArm
    added motor type brushless to constants and made shooterArm

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -138,5 +138,6 @@ public final class Constants {
 
   public static final class NeoMotorConstants {
     public static final double kFreeSpeedRpm = 5676;
+    public static final MotorType kMotorType = MotorType.kBrushless;
   }
 }
---
+++ src/main/java/frc/robot/subsystems/ShooterArm.java
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+
+import frc.robot.Constants;
+import frc.robot.Constants.NeoMotorConstants;
+
+public class ShooterArm extends SubsystemBase {
+
+  private final CANSparkMax m_armShooter = new CANSparkMax(1, NeoMotorConstants.kMotorType);
+
+  /** Creates a new ShooterArm. */
+  public ShooterArm() {}
+
+  public void aimUp() {}
+
+  public void aimDown() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
