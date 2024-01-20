07f8ba5f684a61cf44ea8b6d6238fbf62bf9de7f
madpikachu97 <johnadare3@gmail.com>
2024-01-20 12:10:17 -0600

    update actuator in the commands

--- src/main/java/frc/robot/commands/ActuatorUp.java
+++ src/main/java/frc/robot/commands/ActuatorUp.java
@@ -10,11 +10,13 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class ActuatorUp extends InstantCommand {
-  public ActuatorUp() {
+  public ActuatorUp(Pneumatics pneumatics) {
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    pneumatics.Up();
+  }
 }
