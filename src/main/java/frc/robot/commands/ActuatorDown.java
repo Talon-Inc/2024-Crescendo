b16c83e877dd228e64c36d9b7454ff38f4894cec
Belal-Alchahrour <belal2005alchahrour000@gmail.com>
2024-01-20 12:35:01 -0600

    updated the command

--- src/main/java/frc/robot/commands/ActuatorDown.java
+++ src/main/java/frc/robot/commands/ActuatorDown.java
@@ -5,12 +5,16 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Pneumatics;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class ActuatorDown extends InstantCommand {
+  Pneumatics pneumatics;
+
   public ActuatorDown(Pneumatics pneumatics) {
+    this.pneumatics = pneumatics;
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
--- src/main/java/frc/robot/commands/ActuatorUp.java
+++ src/main/java/frc/robot/commands/ActuatorUp.java
@@ -5,12 +5,15 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Pneumatics;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class ActuatorUp extends InstantCommand {
+  Pneumatics pneumatics;
   public ActuatorUp(Pneumatics pneumatics) {
+    this.pneumatics = pneumatics;
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
