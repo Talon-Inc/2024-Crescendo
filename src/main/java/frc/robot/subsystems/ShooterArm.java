ad5faa84b50ebc2b78f849d62e546f75db005eb3
Kylejackson27 <kyle.jackson480@gmail.com>
2024-01-20 11:08:21 -0600

    Update ShooterArm.java

--- src/main/java/frc/robot/subsystems/ShooterArm.java
+++ src/main/java/frc/robot/subsystems/ShooterArm.java
@@ -7,7 +7,6 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 
-import frc.robot.Constants;
 import frc.robot.Constants.ShooterConstants;
 
 public class ShooterArm extends SubsystemBase {
