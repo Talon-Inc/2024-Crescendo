1d7b1665895abfcf6239d7ca5b33faee92d754cb
Shawn <shawn.s.ali@cloud.redoakisd.org>
2024-01-23 18:07:01 -0600

    Reworked LED subsystem

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -6,9 +6,29 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+
+
 public class LED extends SubsystemBase {
+  private final PWMSparkMax led = new PWMSparkMax(LED_CHANNEL); 
+  private double color = 0;
+
   /** Creates a new LED. */
   public LED() {}
+
+  public void setGreen() {
+    color = 0.77;
+    led.set(color);
+  }
+
+  public void setBlack() {
+    color = 0.99;
+    led.set(color);
+  }
+
+  public double color() {
+    return color;
+  }
 
   @Override
   public void periodic() {
