fa5deb32ce83fd089788cb6e6e76f742d42fe85b
Belal-Alchahrour <belal2005alchahrour000@gmail.com>
2024-02-17 13:30:51 -0600

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -97,7 +97,7 @@ public final class Constants {
 
     // { FrontLeft, FrontRight, RearLeft, RearRight }
     public static final double[] kDrivingP = { 0.18, 0.10, 0.16, 0.16 };
-    public static final double[] kDrivingI = { 0.0001, 0.0001, 0.0001, 0.0001 };
+    public static final double[] kDrivingI = { 0.000, 0.000, 0.000, 0.000 };
     public static final double[] kDrivingD = { 0.05, 0.06, 0.05, 0.05 };
     public static final double kDrivingFF = 1 / kDriveWheelFreeSpeedRps;
     public static final double kDrivingMinOutput = -1;
