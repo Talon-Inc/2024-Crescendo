55a1dd16691a1a54f1bdb4c705ae1fbb04973c96
Belal-Alchahrour <belal2005alchahrour000@gmail.com>
2024-03-19 18:51:37 -0500

    changed default shooter speed

--- src/main/java/frc/robot/commands/IntakeNote.java
+++ src/main/java/frc/robot/commands/IntakeNote.java
@@ -35,7 +35,7 @@ public class IntakeNote extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    shooter.shootSpeaker();
+    shooter.shootAmp();
     intake.stop();
     led.setGold();
   }
