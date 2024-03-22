195d1449f49fd7ac3cf4403ef1ffb3bcc489fc49
Jonathan Labrum <jonathan.d.labrum@gmail.com>
2024-03-22 17:07:38 -0500

    Update intake speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -53,7 +53,7 @@ public final class Constants {
     public static final int kCurrentLimit = 40;
 
     // Intake speed
-    public static final double kSpeed = 0.5;
+    public static final double kSpeed = 0.47;
 
     // Photo sensor port
     public static final int kSensorAnalogPort = 0;
